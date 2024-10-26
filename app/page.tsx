9f4c1cfef3bfb272842bc204c4e69be1a0bb0905
ThugRaven <kamilo1715@wp.pl>
2024-10-26 19:33:18 +0200

    feat: update styles

--- app/page.tsx
+++ app/page.tsx
@@ -113,35 +113,31 @@ export default function Home() {
 	// }, [handleKeyDown]);
 
 	return (
-		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-			<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-				<div className="font-mono">
-					{/* <div>
+		<div className="font-mono">
+			{/* <div>
 						{new Intl.DateTimeFormat('en-US', {
 							timeStyle: 'full',
 						}).format(endTime - startTime)}
 					</div> */}
-					{/* <div>
+			{/* <div>
 						{new Date((endTime - startTime) / 1000).toLocaleTimeString()}
 					</div> */}
-					{/* <div>Correct chars: {correctChars}</div>
+			{/* <div>Correct chars: {correctChars}</div>
 					<div>Incorrect chars: {incorrectChars}</div>
 					<TypedText text={word} index={index} errorsArray={errorsArray} /> */}
-					{isGoalReached && (
-						<div className="absolute inset-0 flex items-center justify-center bg-black/85 z-10">
-							<span className="text-6xl">You{"'"}ve won!</span>
-						</div>
-					)}
-					<TypingController onMove={handleOnMove} width={width} pos={pos}>
-						<div ref={canvasContainer} className="w-full h-full ">
-							<canvas ref={canvasRef} className=""></canvas>
-						</div>
-						{/* <div className="grid grid-cols-10 grid-rows-10">{maze}</div> */}
-						{/* className="w-36 h-36" */}
-					</TypingController>
-					{/* <TypingContainer></TypingContainer> */}
+			{isGoalReached && (
+				<div className="absolute inset-0 flex items-center justify-center bg-black/85 z-10">
+					<span className="text-6xl">You{"'"}ve won!</span>
 				</div>
-			</main>
+			)}
+			<TypingController onMove={handleOnMove} width={width} pos={pos}>
+				<div ref={canvasContainer} className="w-full h-full ">
+					<canvas ref={canvasRef} className=""></canvas>
+				</div>
+				{/* <div className="grid grid-cols-10 grid-rows-10">{maze}</div> */}
+				{/* className="w-36 h-36" */}
+			</TypingController>
+			{/* <TypingContainer></TypingContainer> */}
 		</div>
 	);
 }
